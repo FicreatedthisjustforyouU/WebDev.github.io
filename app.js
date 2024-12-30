9e864da6728e835616c24fb78ec7c0b8ffa9b827
Zachary Ian Bautista <zacharyianbautista1122@gmail.com>
2024-12-30 16:38:11 +0800

    added deets

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }   else{
-            entry.target.classList.remove('show');
-        }
-    });
-});
+let sections = document.querySelectorAll("section");
 
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 250;
+    let height = sec.offsetHeight;
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show");
+    } else {
+      sec.classList.remove("show");
+    }
+  });
+};
